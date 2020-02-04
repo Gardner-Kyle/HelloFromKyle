297fa47f626d346c81e4655079389abe6efa5379
scott <scottcurrell@gmail.com>
2020-02-03 21:22:27 -0700

    add greeting from Scott

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -1,5 +1,6 @@
 public class HelloMain {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Kyle");
+        System.out.println("Hello, Kyle. My name is Scott.");
     }
 }
